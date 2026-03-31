66b433d00386e4061ba108b0ce80b7a91141ef3c
torriankell <vadimpropvp17@gmail.com>
2026-03-31 15:51:15 +0700

    lab 3

---
+++ Lab1w/Program.cs
@@ -0,0 +1,29 @@
+﻿using Lab1w;
+
+Console.WriteLine("Enter student`s name, topic, date (YYYY.MM.DD):");
+
+string[] rawData;
+DateTime dt;
+
+while (true)
+{
+    rawData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    if (rawData.Length != 3) {
+        Console.WriteLine("Wrong params number. Try again:");
+        continue;
+    }
+
+    if (DateTime.TryParseExact(rawData[2], "yyyy.MM.dd",
+       System.Globalization.CultureInfo.InvariantCulture,
+       System.Globalization.DateTimeStyles.None, out dt))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Error: Invalid date format. Please use YYYY.MM.DD format. Try again:");
+    }
+
+}
+
+Topic topic = new Topic(rawData[0], rawData[1], dt);
