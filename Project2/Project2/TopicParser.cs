bf03f5c257b469e506bace882ce0c2ae91e33e42
torriankell <vadimpropvp17@gmail.com>
2026-04-20 09:28:13 +0700

    Update again 1

---
+++ Project2/Project2/TopicParser.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Controls;
+
+namespace Project2
+{
+    public class TopicParser
+    {
+        private const char SEPARATOR = ';';
+
+        public static Topic? BuildTopicFromStrings(string topic)
+        {
+            if (string.IsNullOrEmpty(topic))
+                throw new ArgumentException("Строка пуста");
+
+            string[] buffer = topic.Split(SEPARATOR);
+            if (buffer.Length != 3)
+                throw new FormatException($"Ожидалось 3 поля, получено {buffer.Length}");
+
+            if (string.IsNullOrWhiteSpace(buffer[0]))
+                throw new ArgumentException("Имя студента пусто");
+
+            if (string.IsNullOrWhiteSpace(buffer[1]))
+                throw new ArgumentException("Название темы пусто");
+
+            if (!DateOnly.TryParseExact(buffer[2].Trim(), "yyyy.MM.dd",
+                System.Globalization.CultureInfo.InvariantCulture,
+                System.Globalization.DateTimeStyles.None, out DateOnly date))
+                throw new FormatException($"Неверный формат даты: {buffer[2]}");
+
+            string stuName = buffer[0].Trim();
+            string topicName = buffer[1].Trim();
+
+            return new Topic(stuName, topicName, date);
+        }
+    }
+}
---
+++ Project2/Project2/TopicViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Project2
+{
+    public class TopicViewModel : INotifyPropertyChanged
+    {
+        private Topic _topic;
+        public ObservableCollection<Topic> topics { get; set; }
+        public Topic selectedTopic
+        {
+            get
+            {
+                return selectedTopic;
+            }
+            set
+            {
+                selectedTopic = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public TopicViewModel(Topic topic) => topics?.Add(topic);
+        
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
